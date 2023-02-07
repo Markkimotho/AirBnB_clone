6fe893b85e7170a656c54f9a9070014bed2765ac
MarkTinega <martinozas@gmail.com>
2023-02-07 10:55:03 +0300

    added the base_model.py file that define BaseModel Class

---
+++ models/base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" A module that defines the "BaseModel" class """
+
+class BaseModel:
+    """class BaseModel:
+    -Defines all common attributes/methods for other classes
+    """
